8b792388007c3e10826e34756411f771346ba168
Ali Lotfi <91953142+alilotfi23@users.noreply.github.com>
2024-09-06 12:38:17 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,28 @@
+# Define a Kubernetes namespace for Jenkins
 resource "kubernetes_namespace" "jenkins" {
   metadata {
+    # Set the name of the namespace to "jenkins"
     name = "jenkins"
+    
+    # Define labels for the namespace for identification and organization
     labels = {
-      name        = "jenkins"
-      description = "jenkins"
+      name        = "jenkins"          # The name label for the namespace
+      description = "jenkins"          # A description label for the namespace
     }
   }
 }
+
+# Define a Helm release for deploying Jenkins
 resource "helm_release" "jenkins" {
-  name       = "jenkins"
-  repository = "https://charts.jenkins.io"
-  chart      = "jenkins"
-  version    = "4.2.17"
-  namespace  = "jenkins"
-  timeout    = 300
+  name       = "jenkins"                            # Name of the Helm release
+  repository = "https://charts.jenkins.io"         # Helm chart repository URL
+  chart      = "jenkins"                            # Name of the Helm chart to deploy
+  version    = "4.2.17"                             # Specific version of the Helm chart to use
+  namespace  = kubernetes_namespace.jenkins.metadata[0].name  # Namespace where Jenkins will be deployed
+  timeout    = 300                                   # Timeout in seconds for the Helm release operation
+
+  # Specify the values file for customizing the Helm chart deployment
   values = [
-    file("values.yaml"),
+    file("values.yaml"),                             # Path to the values file containing configuration settings
   ]
 }
